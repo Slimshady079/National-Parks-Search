2d1163d319e94ee06f2d150899e8f4c38507c514
Jackson Grimm <92557509+Bdogy@users.noreply.github.com>
2022-11-09 14:36:43 -0700

    fixed comment on line 18

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,9 +14,8 @@ var stateSort = function (response) {
     //loop goes through every item in the parks arr inside of Data
     for (let x = 0; x < response.data[i].parks.length; x++) {
       //checks if the parks state code is = to colorado if so then push park fullName into an arr and park actives call .name
-
       if (response.data[i].parks[x].states === "CO") {
-        // if state park code = CO then create new
+        // if state park code = CO then create new object and push to park arr
         parksArr.push({
           attraction: [].concat(response.data[i].name),
           parkState: response.data[i].parks[x].states,
