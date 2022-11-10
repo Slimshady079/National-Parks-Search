49c25fb2954486519a8faf4b9c2f5e99c4255408
Jackson Grimm <92557509+Bdogy@users.noreply.github.com>
2022-11-10 14:01:46 -0700

    all current park info is shown when user searches for result

--- assets/js/resultsScript.js
+++ assets/js/resultsScript.js
@@ -1,22 +1,31 @@
 results = JSON.parse(localStorage.getItem("search"));
 console.log(results);
 
-var box = $("#resultBox");
-var card = $("<div>");
-var cardName = $("<p>");
-var state = $("<p>");
-var code = $("<p>");
-var url = $("<a>");
+for (let i = 0; i < results.length; i++) {
+  var box = $("#resultBox");
+  var card = $("<div>");
+  var cardName = $("<p>");
+  var state = $("<p>");
+  var code = $("<p>");
+  var url = $("<a>");
+  var list = $("<ul>");
+  cardName.text(results[i].parkName);
+  state.text(results[i].parkState);
+  code.text(results[i].parkCode);
+  url.text(results[i].parkURL);
+  url.attr("href", results[i].parkURL);
 
-cardName.text(results[0].parkName);
-state.text(results[0].parkState);
-code.text(results[0].parkCode);
-url.text(results[0].parkURL);
-url.attr("href", results[0].parkURL);
-
-card.append(cardName);
-card.append(state);
-card.append(code);
-card.append(url);
-
-box.append(card);
+  card.append(cardName);
+  card.append(state);
+  card.append(code);
+  card.append(url);
+  card.append(list);
+  //loop for attractions
+  for (let x = 0; x < results[i].attraction.length; x++) {
+    listEl = $("<li>");
+    listEl.text(results[i].attraction[x]);
+    list.append(listEl);
+  }
+  card.append(list);
+  box.append(card);
+}
