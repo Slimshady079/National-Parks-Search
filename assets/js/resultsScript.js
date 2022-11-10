40c8072489882f9e3ac7199cee8fe16f6cfeb4c3
Jackson Grimm <92557509+Bdogy@users.noreply.github.com>
2022-11-10 13:52:04 -0700

    all park info besides activitys is displayed on screen

--- assets/js/resultsScript.js
+++ assets/js/resultsScript.js
@@ -4,6 +4,19 @@ console.log(results);
 var box = $("#resultBox");
 var card = $("<div>");
 var cardName = $("<p>");
+var state = $("<p>");
+var code = $("<p>");
+var url = $("<a>");
+
 cardName.text(results[0].parkName);
+state.text(results[0].parkState);
+code.text(results[0].parkCode);
+url.text(results[0].parkURL);
+url.attr("href", results[0].parkURL);
+
 card.append(cardName);
+card.append(state);
+card.append(code);
+card.append(url);
+
 box.append(card);
