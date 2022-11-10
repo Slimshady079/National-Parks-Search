84a5ba70e57f82058dc12bc5f4b5e0682b619e0c
Jackson Grimm <92557509+Bdogy@users.noreply.github.com>
2022-11-10 13:46:06 -0700

    park name is now appened to page

--- assets/js/resultsScript.js
+++ assets/js/resultsScript.js
@@ -1,2 +1,9 @@
 results = JSON.parse(localStorage.getItem("search"));
 console.log(results);
+
+var box = $("#resultBox");
+var card = $("<div>");
+var cardName = $("<p>");
+cardName.text(results[0].parkName);
+card.append(cardName);
+box.append(card);
