680105d98bf4f250afd5dbb42e51732637194574
Jackson Grimm <92557509+Bdogy@users.noreply.github.com>
2022-11-12 13:02:22 -0700

    fixed styling bug by building cards inside of function instead of loop

--- assets/js/resultsScript.js
+++ assets/js/resultsScript.js
@@ -2,7 +2,7 @@ results = JSON.parse(localStorage.getItem("search"));
 console.log(results);
 
 //pulled from my past project -Jackson Grimm
-function currentWeather(lat, lon) {
+function currentWeather(lat, lon, i) {
   var url =
     "https://api.openweathermap.org/data/2.5/weather?lat=" +
     lat +
@@ -17,15 +17,15 @@ function currentWeather(lat, lon) {
       console.log(data);
       var temp = $("<p>");
       temp.text("temperature " + data.main.temp + "F");
-      card.append(temp);
-      return temp;
+      buildCards(results, temp, i);
     });
 }
 
-for (let i = 0; i < results.length; i++) {
-  var box = $("#resultBox");
+function buildCards(results, temp, i) {
   var card = $("<div>");
+  var box = $("#resultBox");
 
+  //creates elements and adds classes
   card.addClass("card");
   var cardName = $("<p>");
   cardName.addClass("cardName");
@@ -41,14 +41,7 @@ for (let i = 0; i < results.length; i++) {
   var description = $("<p>");
   description.addClass("desc");
 
-  //   <iframe src="https://embed.waze.com/iframe?zoom=12&lat=45.6906304&lon=-120.810983"
-  //   width="300" height="400"></iframe>
-
-  var lat = results[i].lat;
-  var long = results[i].long;
-  //calls weather function with weather API
-  currentWeather(lat, long);
-
+  //adds text content to elements inside card
   cardName.text(results[i].parkName);
   state.text("State Code: " + results[i].parkState);
   code.text("Park Code: " + results[i].parkCode);
@@ -58,6 +51,7 @@ for (let i = 0; i < results.length; i++) {
   img.attr("src", results[i].img);
   description.text(results[i].description);
 
+  //waze frame
   var WazeSrc =
     "https://embed.waze.com/iframe?zoom=12&lat=" + lat + "&lon=" + long;
   var wazeFrame = $(
@@ -66,13 +60,15 @@ for (let i = 0; i < results.length; i++) {
   wazeFrame.attr("id", "wazeFrame");
   wazeFrame.attr("loading", "lazy");
   wazeFrame.addClass("wazeFrame");
-  card.addClass("cards");
+
   //card build
+  card.addClass("cards");
   card.append(cardName);
   card.append(img);
   card.append(description);
   card.append(list);
   card.append(state);
+  card.append(temp);
   card.append(code);
   card.append(url);
   card.append(wazeFrame);
@@ -84,4 +80,66 @@ for (let i = 0; i < results.length; i++) {
   }
   card.append(list);
   box.append(card);
+}
+
+for (let i = 0; i < results.length; i++) {
+  //creates container for cards and cards el
+  var lat = results[i].lat;
+  var long = results[i].long;
+  //calls weather function with weather API
+  currentWeather(lat, long, i);
+
+  // card.addClass("card");
+  // var cardName = $("<p>");
+  // cardName.addClass("cardName");
+  // var state = $("<p>");
+  // state.addClass("state");
+  // var code = $("<p>");
+  // code.addClass("code");
+  // var url = $("<a>");
+  // url.addClass("url");
+  // var list = $("<ul>");
+  // list.addClass("list");
+  // var img = $("<img>");
+  // var description = $("<p>");
+  // description.addClass("desc");
+
+  // //   <iframe src="https://embed.waze.com/iframe?zoom=12&lat=45.6906304&lon=-120.810983"
+  // //   width="300" height="400"></iframe>
+
+  // cardName.text(results[i].parkName);
+  // state.text("State Code: " + results[i].parkState);
+  // code.text("Park Code: " + results[i].parkCode);
+  // url.text(results[i].parkURL);
+  // url.attr("href", results[i].parkURL);
+  // url.text("Park Webpage");
+  // img.attr("src", results[i].img);
+  // description.text(results[i].description);
+
+  // var WazeSrc =
+  //   "https://embed.waze.com/iframe?zoom=12&lat=" + lat + "&lon=" + long;
+  // var wazeFrame = $(
+  //   "<iframe width ='400' height='600' src=" + WazeSrc + "></iframe>"
+  // );
+  // wazeFrame.attr("id", "wazeFrame");
+  // wazeFrame.attr("loading", "lazy");
+  // wazeFrame.addClass("wazeFrame");
+  // card.addClass("cards");
+  // //card build
+  // card.append(cardName);
+  // card.append(img);
+  // card.append(description);
+  // card.append(list);
+  // card.append(state);
+  // card.append(code);
+  // card.append(url);
+  // card.append(wazeFrame);
+  // //loop for attractions
+  // for (let x = 0; x < results[i].attraction.length; x++) {
+  //   listEl = $("<li>");
+  //   listEl.text(results[i].attraction[x]);
+  //   list.append(listEl);
+  // }
+  // card.append(list);
+  // box.append(card);
 }
