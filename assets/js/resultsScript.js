6fdfadd842b892dafacc3677f3c06b4b012ea818
Jackson Grimm <92557509+Bdogy@users.noreply.github.com>
2022-11-12 12:47:49 -0700

    weather for location is shown on park cards

--- assets/js/resultsScript.js
+++ assets/js/resultsScript.js
@@ -1,9 +1,31 @@
 results = JSON.parse(localStorage.getItem("search"));
 console.log(results);
 
+//pulled from my past project -Jackson Grimm
+function currentWeather(lat, lon) {
+  var url =
+    "https://api.openweathermap.org/data/2.5/weather?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&units=imperial&appid=569d785adfe9b44db482c835162b2e7a";
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      var temp = $("<p>");
+      temp.text("temperature " + data.main.temp + "F");
+      card.append(temp);
+      return temp;
+    });
+}
+
 for (let i = 0; i < results.length; i++) {
   var box = $("#resultBox");
   var card = $("<div>");
+
   card.addClass("card");
   var cardName = $("<p>");
   cardName.addClass("cardName");
@@ -24,6 +46,9 @@ for (let i = 0; i < results.length; i++) {
 
   var lat = results[i].lat;
   var long = results[i].long;
+  //calls weather function with weather API
+  currentWeather(lat, long);
+
   cardName.text(results[i].parkName);
   state.text("State Code: " + results[i].parkState);
   code.text("Park Code: " + results[i].parkCode);
@@ -32,6 +57,7 @@ for (let i = 0; i < results.length; i++) {
   url.text("Park Webpage");
   img.attr("src", results[i].img);
   description.text(results[i].description);
+
   var WazeSrc =
     "https://embed.waze.com/iframe?zoom=12&lat=" + lat + "&lon=" + long;
   var wazeFrame = $(
